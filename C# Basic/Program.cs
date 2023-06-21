afbb24ca9fcb52d71999e63388ad2c5d4a6cdf08
das972811 <salasdiego54@gmail.com>
2023-06-21 16:23:52 -0500

    Type conversion

--- C# Basic/Program.cs
+++ C# Basic/Program.cs
@@ -6,4 +6,9 @@
 
 Console.WriteLine("Welcome to the cat food store!");
 string? entry = Ask("How many cans are you ordering?");
-Console.WriteLine($"You entered {entry} cans");
+// Console.WriteLine($"You entered {entry} cans");
+
+int i;
+int.TryParse(entry, out i);
+
+Console.WriteLine($"For {entry} cans, your total is: ${i * 2}");
