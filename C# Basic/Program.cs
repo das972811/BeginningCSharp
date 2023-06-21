808b5065c2be0b4d298004cf1e2a81a15f4cc389
das972811 <salasdiego54@gmail.com>
2023-06-21 15:49:43 -0500

    String interpolation

--- C# Basic/Program.cs
+++ C# Basic/Program.cs
@@ -1,9 +1,9 @@
 ﻿static string? Ask(string question)
 {
-    Console.Write(question);
+    Console.Write(question + " ");
     return Console.ReadLine();
 }
 
 Console.WriteLine("Welcome to the cat food store!");
 string? entry = Ask("How many cans are you ordering?");
-Console.WriteLine(entry);
+Console.WriteLine($"You entered {entry} cans");
