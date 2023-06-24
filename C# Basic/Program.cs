9765636397081cb5621abda515098fa125bd6315
das972811 <salasdiego54@gmail.com>
2023-06-24 17:20:28 -0500

    Added an if statment

--- C# Basic/Program.cs
+++ C# Basic/Program.cs
@@ -4,11 +4,17 @@
     return Console.ReadLine();
 }
 
+static double Price(int quantity) {
+    return quantity * 2;
+}
+
 Console.WriteLine("Welcome to the cat food store!");
 string? entry = Ask("How many cans are you ordering?");
 // Console.WriteLine($"You entered {entry} cans");
 
-int i;
-int.TryParse(entry, out i);
+int num;
+int.TryParse(entry, out num);
 
-Console.WriteLine($"For {entry} cans, your total is: ${i * 2}");
+double total = (double) Price(num);
+
+Console.WriteLine($"For {num} cans, your total is: ${total}");
