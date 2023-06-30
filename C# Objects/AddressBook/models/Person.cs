668e998b90e46be805489b95e3248c288e9c1597
das972811 <salasdiego54@gmail.com>
2023-06-30 15:36:51 -0500

    Added member properties to Person class

--- C# Objects/AddressBook/models/Person.cs
+++ C# Objects/AddressBook/models/Person.cs
@@ -2,6 +2,14 @@ namespace AddressBook.Model
 {
     public class Person : Human
     {
-        
+        public string MailingAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int PostalCode { get; set; }
+
+        public Person() : base()
+        {
+
+        }
     }
 }
