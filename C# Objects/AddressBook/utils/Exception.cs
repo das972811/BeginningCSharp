d689a870970fa19bf5804b728ba25527565c3700
das972811 <salasdiego54@gmail.com>
2023-07-03 16:28:53 -0500

    Updated Exceptions

--- C# Objects/AddressBook/utils/Exception.cs
+++ C# Objects/AddressBook/utils/Exception.cs
@@ -2,6 +2,23 @@ namespace AddressBook.Utils
 {
     class AddressBookException : Exception
     {
-        
+        public AddressBookException()
+        {
+        }
+
+        public AddressBookException(string message) : base(message)
+        {
+        }
+    }
+
+    class OutOfBoundsAddressException : AddressBookException
+    {
+        public OutOfBoundsAddressException()
+        {
+        }
+
+        public OutOfBoundsAddressException(string message) : base(message)
+        {
+        }
     }
 }
