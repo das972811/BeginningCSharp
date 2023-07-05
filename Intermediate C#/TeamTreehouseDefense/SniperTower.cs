257c4f7ac8302e5516b87afa3d357e0fc88ea322
das972811 <salasdiego54@gmail.com>
2023-07-05 15:27:25 -0500

    Updated

--- Intermediate C#/TeamTreehouseDefense/SniperTower.cs
+++ Intermediate C#/TeamTreehouseDefense/SniperTower.cs
@@ -2,6 +2,8 @@ namespace TreehouseDefense
 {
     class SniperTower : Tower
     {
+        protected override int Range { get; } = 2;
+
         protected override double Accuracy { get; } = .90;
 
         public SniperTower (MapLocation mapLocation) : base(mapLocation)
