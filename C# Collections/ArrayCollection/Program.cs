58162693cdc6990c096d0ba5b182c404f6c26c78
das972811 <salasdiego54@gmail.com>
2023-08-01 10:06:19 -0500

    Hash Set

--- C# Collections/ArrayCollection/Program.cs
+++ C# Collections/ArrayCollection/Program.cs
@@ -4,30 +4,18 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        // int[] lockCombination = new int[3];
-        // lockCombination = new int[] {10, 5, 32};
-
-        // int last = lockCombination[2];
-        // Console.WriteLine(last);
-
-        // Cell[][] sheet = new Cell[100][];
-        // sheet[0] = new Cell[10];
-        // sheet[0][0]  = new Cell();
-        
-        // Console.WriteLine(sheet[0][0]);
-
-        int size = 5;
-
-        List<int> myList = new List<int>();
-
-        for (int i = 0; i < size; i++)
+        List<Student> students = new List<Student>
         {
-            myList.Add((int) System.Math.Pow(2, i));
-        }
+            new Student() { Name = "Diego", GradeLevel = 3 },
+            new Student() { Name = "Bob", GradeLevel = 3 },
+            new Student() { Name = "Alicia", GradeLevel = 2 }
+        };
 
-        foreach (int i in myList)
+        students.Sort();
+
+        foreach(Student student in students)
         {
-            Console.WriteLine(i);
+            Console.WriteLine($"{student.Name} is in grade {student.GradeLevel}");
         }
     }
 }
